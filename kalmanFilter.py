38a8b39070fac3c2c4898c36ddc60e3018f1fb1b
gasparnori <nori.nagyonsok@gmail.com>
2018-05-03 21:11:04 +0300

    predictions

--- kalmanFilter.py
+++ kalmanFilter.py
@@ -1,39 +1,93 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
 import sys, random
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 import math
-num_variables=6
-forget= 0.6 #forgetting factor
+import random
+num_variables=4
+forget= 0.1 #forgetting factor
+
+class Plotter():
+    def __init__(self):
+        self.updatedX=[]
+        self.measuredX=[]
+        self.updatedY=[]
+        self.measuredY=[]
+        self.times=[]
+
+
+    def add_updated(self, measured, updated, dt):
+        self.updatedX.append(updated[0])
+        self.updatedY.append(updated[1])
+        self.measuredX.append(measured[0])
+        self.measuredY.append(measured[1])
+        if len(self.times)>0:
+            t=self.times[-1]+dt
+        else:
+            t=dt
+        self.times.append(t)
+
+    def plot(self, adaptive=False):
+        plt.clf()
+        plt.close()
+        titletxt = "Smoothing with adaptive covariances" if adaptive else "Smoothing with fixed covariances"
+        p1=plt.subplot(211)
+        plt.title(titletxt)
+        #plt.xlabel("time (ms)")
+        plt.ylabel("coordinate X (pixel)")
+        ux, =plt.plot(self.times, self.updatedX, 'g', label='Updated')
+        mx, =plt.plot(self.times, self.measuredX, 'r', label='Measured')
+        plt.legend(handles=[ux, mx])
+        #plt.setp(p1.get_xticklabels(), visible=False)
+        p2= plt.subplot(212, sharex=p1)  # creates 2nd subplot with yellow background
+        plt.xlabel("time (ms)")
+        plt.ylabel("coordinate Y (pixel)")
+        uy, =plt.plot(self.times, self.updatedY, 'g', label='Updated')
+        my, =plt.plot(self.times, self.measuredY, 'r',  label='Measured')
+        plt.legend(handles=[uy, my])
+        plt.show()
+
+    def reset(self):
+        self.updatedX = []
+        self.measuredX = []
+        self.updatedY = []
+        self.measuredY = []
+        self.times = []
 
 
 class Window(QWidget):
 
     def __init__(self):
+        # every 100 times, 5-10 coordinates are missing
+        self.missing_counter=100
         super(Window, self).__init__()
         #self.layout = QVBoxLayout(self)
         self.layout =  QGridLayout(self)
         #self.smoothbtn = QPushButton('Smooth trajectory', self)
-        self.adaptbtn=QRadioButton('Adaptive Filtering', self)
-        self.rollingbtn=QRadioButton('Rolling Covariance', self)
-        self.fixedbtn=QRadioButton('Fixed parameters', self)
-        self.predictbtn= QPushButton('Reset', self)
+        self.adaptbtn=QCheckBox('Adaptive Filtering', self)
+        self.predictbtn=QCheckBox('Predict missing values', self)
+       # self.fixedbtn=QRadioButton('Fixed parameters', self)
+        self.resetbtn= QPushButton('Reset', self)
+        self.plotbtn = QPushButton('Plot results', self)
         self.drawing= QWidget(self)
+        self.plotter=Plotter()
 
         #self.layout.addWidget(self.smoothbtn, 0, 0)
         self.layout.addWidget(self.adaptbtn, 0, 0)
-        self.layout.addWidget(self.rollingbtn, 1, 0)
-        self.layout.addWidget(self.predictbtn, 1, 1)
-        self.layout.addWidget(self.fixedbtn, 2, 0)
+        self.layout.addWidget(self.resetbtn, 0, 1)
+        self.layout.addWidget(self.predictbtn, 1, 0)
+        #self.layout.addWidget(self.fixedbtn, 0, 1)
+        self.layout.addWidget(self.plotbtn, 1, 1)
         self.layout.addWidget(self.drawing, 2, 1)
         self.resetFilter()
-
 
         self.initUI()
 
     def resetFilter(self):
+        self.plotter.reset()
         self.timer = QElapsedTimer()
         #to draw
         self.points_final = []
@@ -51,12 +105,12 @@ class Window(QWidget):
         #self.Fk =np.array( [[1, 0, deltaT, 0],[0, 1, 0, deltaT], [0, 0, 1, 0], [0, 0, 0, 1]])
 
         #Fk: transition matrix for acceleration, velocity and position
-        self.Fk = np.matrix(((1, 0, deltaT, 0, 0.5*deltaT*deltaT, 0),
-                            (0, 1, 0, deltaT, 0, 0.5*deltaT*deltaT),
-                            (0, 0, 1, 0, deltaT, 0),
-                            (0, 0, 0, 1, 0, deltaT),
-                            (0, 0, 0, 0, 1,0),
-                            (0, 0, 0, 0, 0, 1)))
+        # self.Fk = np.matrix(((1, 0, deltaT, 0, 0.5*deltaT*deltaT, 0),
+        #                     (0, 1, 0, deltaT, 0, 0.5*deltaT*deltaT),
+        #                     (0, 0, 1, 0, deltaT, 0),
+        #                     (0, 0, 0, 1, 0, deltaT),
+        #                     (0, 0, 0, 0, 1,0),
+        #                     (0, 0, 0, 0, 0, 1)))
         #Hk:observation matrix
         self.Hk =np.eye(num_variables, num_variables) #not going to change
         #Pk: transition covariance
@@ -65,21 +119,17 @@ class Window(QWidget):
         #self.measurement_covariance = np.eye(4, 4)
         self.Rk =np.eye(num_variables, num_variables)* 10  # estimate of measurement variance, change to see effect
         # Q
-        self.Qk = np.matrix([[0.5, 0, 0, 0, 0, 0],
-                    [0, 0.5, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0]])
+        self.Qk = np.matrix([[0.5, 0, 0, 0],#, 0, 0],
+                    [0, 0.5, 0, 0],#, 0, 0],
+                    [0, 0, 0.001, 0],#, 0, 0],
+                    [0, 0, 0, 0.001]])#, 0, 0],
+                   # [0, 0, 0, 0, 0.0001, 0],
+                   # [0, 0, 0, 0, 0, 0.0001]])
         self.Kgain=np.eye(num_variables, num_variables)
         self.kf = None
 
         self.onlinefilter_means = []
         self.onlinefilter_covariance = []
- #   def smoothing(self):
-
- #       self.predictbtn.setChecked(False)
-
 
     def resetGUI(self, state):
         print "restart"
@@ -87,14 +137,14 @@ class Window(QWidget):
         self.timer.start()
         self.update()
 
-
     def initUI(self):
         self.setGeometry(300, 300, 600, 600)
 
         self.layout.setAlignment(Qt.AlignTop)
         #self.smoothbtn.clicked.connect(self.smoothing)
-        self.predictbtn.clicked.connect(self.resetGUI)
-        #self.predictbtn.setCheckable(True)
+        self.resetbtn.clicked.connect(self.resetGUI)
+        self.plotbtn.clicked.connect(lambda: self.plotter.plot(self.adaptbtn.isChecked()))
+
         self.setWindowTitle('Points')
         self.show()
 
@@ -130,39 +180,53 @@ class Window(QWidget):
 
     def addPoints(self, x,y):
         mu, sigma = 0, 10  # mean and standard deviation
-        datax = int(x + np.random.normal(mu, sigma, 1))
-        datay = int(y + np.random.normal(mu, sigma, 1))
         t = self.timer.elapsed()
         # print t
         self.timer.restart()
-        if t>0:
-            vx= (datax-self.points_measured[-1][0])/t
-            vy= (datay-self.points_measured[-1][1])/t
-            ax= (vx-self.measured_state[2,0])/t
-            ay= (vy-self.measured_state[3,0])/t
+
+        if x is None:
+            p = self.iterate_filter(t,
+                                    np.asmatrix(self.updated_state[:, -1]).transpose(),
+                                    None,
+                                    self.adaptbtn.isChecked(),
+                                    self.predictbtn.isChecked())
+            self.updated_state[:, :-1]=self.updated_state[:, 1:]
+            self.updated_state[:, -1]=p.A1
+            self.measured_state = self.updated_state[:, -1]
+            self.measured_state.shape = (num_variables, 1)
+            return
         else:
-            vx= (self.measured_state[2,0])
-            vy= (self.measured_state[3,0])
-            ax= (self.measured_state[4,0])
-            ay= (self.measured_state[5,0])
+            if self.measured_state is None:
+                self.measured_state=self.updated_state[:, -1]
+                self.measured_state.shape=(num_variables,1)
+            #print self.measured_state
+            datax = int(x + np.random.normal(mu, sigma, 1))
+            datay = int(y + np.random.normal(mu, sigma, 1))
 
-        self.measured_state=np.array([[datax], [datay], [vx], [vy], [ax], [ay]])
-        self.points_measured.append([datax, datay])
+            if t>0:
+                vx= (datax-self.measured_state[0,0])/t
+                vy= (datay-self.measured_state[1,0])/t
+                ax= (vx-self.measured_state[2,0])/t
+                ay= (vy-self.measured_state[3,0])/t
+                print "ax, ay calculation:", ax, ay
+            # else:
+            #     vx= (self.measured_state[2,0])
+            #     vy= (self.measured_state[3,0])
+            #     ax= (self.measured_state[4,0])
+            #     ay= (self.measured_state[5,0])
 
-        #if self.predictbtn.isChecked():
-            #important to change it to the matrix form required
-
-        p=self.iterate_filter(t,
+            self.measured_state=np.array([[datax], [datay], [vx], [vy]])#, [ax], [ay]])
+           # print self.measured_state
+            self.points_measured.append([datax, datay])
+            p=self.iterate_filter(t,
                               np.asmatrix(self.updated_state[:, -1]).transpose(),
                               np.asmatrix(self.measured_state),
                               self.adaptbtn.isChecked(),
-                              self.rollingbtn.isChecked(),
-                              self.fixedbtn.isChecked())
-        self.updated_state[:, :-1]=self.updated_state[:, 1:]
+                              self.predictbtn.isChecked())
 
-        self.updated_state[:, -1]=p.A1
-
-           # print "blabla"
+            self.updated_state[:, :-1] = self.updated_state[:, 1:]
+            self.updated_state[:, -1] = p.A1
+            print self.updated_state[:, -1]
 
 
     def mousePressEvent(self, mouse_event):
@@ -170,62 +234,83 @@ class Window(QWidget):
         self.resetFilter()
 
         self.timer.start()
-        initial_state=np.array([[mouse_event.x()], [mouse_event.y()], [0], [0], [0], [0]])
+        initial_state=np.array([[mouse_event.x()], [mouse_event.y()], [0.0001], [0.0001]])#, [0.0001], [0.0001]])
         self.points_measured.append([mouse_event.x(), mouse_event.y()])
-        self.measured_state[:, 0] = initial_state[:, 0]
+        #self.measured_state[:, 0] = initial_state[:, 0]
         #print self.measured_state
         self.updated_state[:, -1]=initial_state[:,0]
        # self.predicted_state.append(self.initial_state)
 
-
     def mouseMoveEvent(self, mouse_event):
         # print "mouse moved", mouse_event.x(), mouse_event.y()
-        self.addPoints(mouse_event.x(), mouse_event.y())
+        if self.predictbtn.isChecked():
+            if self.missing_counter>10:
+                self.missing_counter=self.missing_counter-1
+                self.addPoints(mouse_event.x(), mouse_event.y())
+            elif self.missing_counter>0:
+                self.missing_counter = self.missing_counter - 1
+                self.addPoints(None, None)
+            else:
+                self.missing_counter=100
+                self.addPoints(None, None)
+        else:
+            self.addPoints(mouse_event.x(), mouse_event.y())
+
 
         self.update()
 
     #the filter itself
-    def iterate_filter(self, dt, u, m, adaptive=False, rolling=False, fixed=True):
+    def iterate_filter(self, dt, u, m, adaptive=False, fixed=True):
         # print dt
 
 
        # m = np.asmatrix(m_state)
        # u = np.asmatrix(u_state).transpose()
-        HkT=self.Hk.transpose()
 
-        self.Fk = np.array([[1, 0, dt, 0, 0.5*dt*dt, 0],
-                            [0, 1, 0, dt, 0, 0.5*dt*dt],
-                            [0, 0, 1, 0, dt, 0],
-                            [0, 0, 0, 1, 0,dt],
-                            [0, 0, 0, 0, 1, 0],
-                            [0, 0, 0, 0, 0, 1]])
+        HkT=self.Hk.transpose()
+        self.Fk = np.array([[1, 0, dt, 0], [0, 1, 0, dt], [0, 0, 1, 0], [0, 0, 0, 1]])
+        # self.Fk = np.array([[1, 0, dt, 0, 0.5*dt*dt, 0],
+        #                     [0, 1, 0, dt, 0, 0.5*dt*dt],
+        #                     [0, 0, 1, 0, dt, 0],
+        #                     [0, 0, 0, 1, 0,dt],
+        #                     [0, 0, 0, 0, 1, 0],
+        #                     [0, 0, 0, 0, 0, 1]])
 
         # #prediction step
+
         pred = self.Fk*u
         cov = self.Fk*self.Pk*self.Fk.transpose()+self.Qk
+        #if self.missing_counter<10:
+        #    print pred, u, dt, self.Fk
+       # print "acceleration: ", pred[4,0], pred[5,0], "last measure: ", u[4, 0], u[5, 0]
+        #check if there is a data:
+        if m is None:
+          #  print "predicted:"
+          #  print "velocity: ", pred[2, 0], pred[3, 0], "acceleration: ", pred[4,0], pred[5,0], "last measure: ", u[2, 0], u[3, 0], "\n\n"
+            updateval=pred
+            self.Kgain = cov * HkT * np.linalg.inv(self.Hk * cov * HkT + self.Rk)
+            self.Pk = (np.eye(num_variables, num_variables) - (self.Kgain * self.Hk)) * cov
+            self.plotter.add_updated([0, 0], [updateval[0, 0], updateval[1, 0]], dt)
 
-        #before update
-        diff = m - self.Hk * pred
-        self.Kgain = cov * HkT * np.linalg.inv(self.Hk * cov * HkT + self.Rk)
-        # #adapting Rk and Qk
-        if adaptive:
-            self.Qk = forget * self.Qk + (1 - forget) * self.Kgain * diff * diff.transpose() * self.Kgain.transpose()
+        else:
+            #before update
+            diff = m - self.Hk * pred
+            self.Kgain = cov * HkT * np.linalg.inv(self.Hk * cov * HkT + self.Rk)
+            # #adapting Rk and Qk
+            if adaptive:
+                self.Qk = forget * self.Qk + (1 - forget) * self.Kgain * diff * diff.transpose() * self.Kgain.transpose()
 
-        #update
-        updateval = pred + self.Kgain * diff
-        residual=m-(self.Hk*updateval)
-        self.Pk = (np.eye(num_variables, num_variables) - (self.Kgain * self.Hk)) * cov
+            #update
+            updateval = pred + self.Kgain * diff
+            residual=m-(self.Hk*updateval)
+            self.Pk = (np.eye(num_variables, num_variables) - (self.Kgain * self.Hk)) * cov
 
-        #c=np.cov(residual)
-        if adaptive:
-            self.Rk = forget * self.Rk + (1 - forget) * (residual * residual.T + (self.Hk * self.Pk * HkT))
+            self.plotter.add_updated([m[0,0], m[1,0]], [updateval[0,0], updateval[1,0]], dt)
+            #c=np.cov(residual)
+            if adaptive:
+                self.Rk = forget * self.Rk + (1 - forget) * (residual * residual.T + (self.Hk * self.Pk * HkT))
 
-        if rolling:
-            self.rollingCov=forget*self.rollingCov+(1-forget)*(residual*residual.T)
-            print self.rollingCov
-
-
-
+       # print "\n m: \n", m, "\n u: \n", u, "\n updateval: \n", updateval
         return updateval
         #self.predicted_state.append(pred)
 
